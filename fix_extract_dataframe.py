ffc57efc1eedab7ad753e2850ecd9c4b294b015f
mcarthur21 <mcyuttademccain@yahoo.com>
2021-06-23 05:21:01 +0100

    Update fix_extract_dataframe.py

--- fix_extract_dataframe.py
+++ fix_extract_dataframe.py
@@ -15,7 +15,7 @@ def read_json(json_file: str)->list:
     """
     
     tweets_data = []
-    for tweets in open(json_file,'r'):
+    for tweets in open(json_file,'python'):
         tweets_data.append(json.loads(tweets))
     
     
